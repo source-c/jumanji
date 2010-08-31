fooplugh/jumanji-foo

commit 269fe073a8792afb2b9ca2202985323a7e57fb65

Just update the position in the status bar

jumanji.c
@@ -371,6 +371,7 @@ void set_completion_row_color(GtkBox*, int, int);
 void switch_view(GtkWidget*);
 void statusbar_set_text(const char*);
 void update_status();
+void update_position();
 GtkEventBox* create_completion_row(GtkBox*, char*, char*, gboolean);
 
 Completion* completion_init();
@@ -1257,7 +1258,12 @@ update_status()
     g_free(markup);
   }
 
-  /* update position */
+  update_position();
+}
+
+void
+update_position()
+{
   GtkAdjustment* adjustment = gtk_scrolled_window_get_vadjustment(GET_CURRENT_TAB_WIDGET());
   gdouble view_size         = gtk_adjustment_get_page_size(adjustment);
   gdouble value             = gtk_adjustment_get_value(adjustment);
@@ -3830,7 +3836,7 @@ cb_wv_notify_title(WebKitWebView* wv, GParamSpec* pspec, gpointer data)
 gboolean
 cb_wv_scrolled(GtkAdjustment* UNUSED(adjustment), gpointer UNUSED(data))
 {
-  update_status();
+  update_position();
   return TRUE;
 }
 

source-c/jumanji

commit 6a3abadf431b05e11fb7c3dcdfa8caf5f92fe78a

resolve bug introduced by 269fe073a8792afb2b9ca2202985323a7e57fb65 (Gtk-CRITICA)
Signed-off-by: Maxime de Roucy <maxime.deroucy@gmail.com>

jumanji.c
@@ -1356,6 +1356,9 @@ update_uri()
 void
 update_position()
 {
+  if (gtk_notebook_get_current_page(Jumanji.UI.view) == -1)
+    return;
+
   GtkAdjustment* adjustment = gtk_scrolled_window_get_vadjustment(GET_CURRENT_TAB_WIDGET());
   gdouble view_size         = gtk_adjustment_get_page_size(adjustment);
   gdouble value             = gtk_adjustment_get_value(adjustment);