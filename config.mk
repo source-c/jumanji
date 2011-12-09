963b309e9f91c6214f36c729514d5a08e7293310
Moritz Lipp <mlq@pwmt.org>
2011-12-09 08:54:55 +0100

    Update Makefile

--- config.mk
+++ config.mk
@@ -8,11 +8,11 @@ PREFIX ?= /usr
 MANPREFIX ?= ${PREFIX}/share/man
 
 # libs
-GTK_INC = $(shell pkg-config --cflags gtk+-2.0 webkit-1.0 unique-1.0)
-GTK_LIB = $(shell pkg-config --libs gtk+-2.0 gthread-2.0 webkit-1.0 unique-1.0)
+GTK_INC = $(shell pkg-config --cflags gtk+-2.0 webkit-1.0 unique-1.0 javascriptcoregtk-1.0)
+GTK_LIB = $(shell pkg-config --libs gtk+-2.0 gthread-2.0 webkit-1.0 unique-1.0 javascriptcoregtk-1.0)
 
 INCS = -I. -I/usr/include ${GTK_INC}
-LIBS = -lc ${GTK_LIB} -lpthread
+LIBS = -lc ${GTK_LIB} -lpthread -lm
 
 # flags
 CFLAGS += -std=c99 -pedantic -Wall -Wextra $(INCS)
