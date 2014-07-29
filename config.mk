c3496a82e01249e549d1bd18111e58c4ac935a52
PiXy <ashtoreth@ukr.net>
2014-07-29 12:42:15 +0300

    adopted to my system

--- config.mk
+++ config.mk
@@ -11,8 +11,11 @@ MANPREFIX ?= ${PREFIX}/share/man
 GTK_INC = $(shell pkg-config --cflags gtk+-2.0 webkit-1.0 unique-1.0 javascriptcoregtk-1.0)
 GTK_LIB = $(shell pkg-config --libs gtk+-2.0 gthread-2.0 webkit-1.0 unique-1.0 javascriptcoregtk-1.0)
 
-INCS = -I. -I/usr/include ${GTK_INC}
-LIBS = -lc ${GTK_LIB} -lpthread -lm
+SOUP_INC = $(shell pkg-config --cflags libsoup-2.4)
+SOUP_LIB = $(shell pkg-config --libs libsoup-2.4)
+
+INCS = -I. -I/usr/include ${GTK_INC} ${SOUP_INC}
+LIBS = -lc ${GTK_LIB} ${SOUP_LIB} -lpthread -lm
 
 # flags
 CFLAGS += -std=c99 -pedantic -Wall -Wextra $(INCS)
